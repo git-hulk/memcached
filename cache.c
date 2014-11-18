d1f17a118ba7f0d99eaa67a62efdc3b6ebdd3dac
tianyi4 <tianyi4@staff.sina.com.cn>
2014-11-18 11:44:37 +0800

    MOD: cache.c添加注释

--- cache.c
+++ cache.c
@@ -37,6 +37,7 @@ cache_t* cache_create(const char *name, size_t bufsize, size_t align,
     ret->destructor = destructor;
 
 #ifndef NDEBUG
+    /* debug模式下在object前后加上redzone, 可检查写入错误 */
     ret->bufsize = bufsize + 2 * sizeof(redzone_pattern);
 #else
     ret->bufsize = bufsize;
@@ -48,6 +49,7 @@ cache_t* cache_create(const char *name, size_t bufsize, size_t align,
 static inline void* get_object(void *ptr) {
 #ifndef NDEBUG
     uint64_t *pre = ptr;
+    /* debug模式, pre+1跳过redzone的8个字节 */
     return pre + 1;
 #else
     return ptr;
@@ -72,6 +74,7 @@ void* cache_alloc(cache_t *cache) {
     void *ret;
     void *object;
     pthread_mutex_lock(&cache->mutex);
+    /* 如果cache池子里面还有空闲的object, 直接从池子里面获取, 否则通过malloc申请一个object */
     if (cache->freecurr > 0) {
         ret = cache->ptr[--cache->freecurr];
         object = get_object(ret);
@@ -90,11 +93,14 @@ void* cache_alloc(cache_t *cache) {
     pthread_mutex_unlock(&cache->mutex);
 
 #ifndef NDEBUG
+    /* 如果是debug模式，就在object前后加上redzone */
     if (object != NULL) {
         /* add a simple form of buffer-check */
         uint64_t *pre = ret;
+        /* object头部八个字节变为redzone */
         *pre = redzone_pattern;
         ret = pre+1;
+        /* object尾部八个字节变为redzone */
         memcpy(((char*)ret) + cache->bufsize - (2 * sizeof(redzone_pattern)),
                &redzone_pattern, sizeof(redzone_pattern));
     }
@@ -107,6 +113,7 @@ void cache_free(cache_t *cache, void *ptr) {
     pthread_mutex_lock(&cache->mutex);
 
 #ifndef NDEBUG
+    /* debug模式下需要校验object的头部和尾部8个字节是否为redzone */
     /* validate redzone... */
     if (memcmp(((char*)ptr) + cache->bufsize - (2 * sizeof(redzone_pattern)),
                &redzone_pattern, sizeof(redzone_pattern)) != 0) {
@@ -125,9 +132,11 @@ void cache_free(cache_t *cache, void *ptr) {
     }
     ptr = pre;
 #endif
+    
     if (cache->freecurr < cache->freetotal) {
         cache->ptr[cache->freecurr++] = ptr;
     } else {
+        /* 如果空闲池子的指针数组不够用，申请一片更大的数组来存放 */
         /* try to enlarge free connections array */
         size_t newtotal = cache->freetotal * 2;
         void **new_free = realloc(cache->ptr, sizeof(char *) * newtotal);
